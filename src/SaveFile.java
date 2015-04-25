33491d7430b5b4e3481bf34592e5fbcb60ebf370
KeithRuss <keith.m.russ@gmail.com>
2015-04-25 00:41:05 -0400

    Finishing up the SaveFile class

--- src/SaveFile.java
+++ src/SaveFile.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
@@ -50,23 +52,24 @@ public class SaveFile
 		while(contents != null) 
 		{
 			StringTokenizer tk = new StringTokenizer(contents);
-			if(tk.nextToken().equals("Username"))
+			String type = tk.nextToken();
+			if(type.equals("Username"))
 				name = tk.nextToken();
-			else if(tk.nextToken().equals("Password"))
+			else if(type.equals("Password"))
 				password = tk.nextToken();
-			else if(tk.nextToken().equals("Color"))
+			else if(type.equals("Color"))
 				color = tk.nextToken();
-			else if(tk.nextToken().equals("Endless_easy"))
+			else if(type.equals("Endless_easy"))
 				endlesseasy = Integer.parseInt(tk.nextToken());
-			else if(tk.nextToken().equals("Endless_medium"))
+			else if(type.equals("Endless_medium"))
 				endlessmed = Integer.parseInt(tk.nextToken());
-			else if(tk.nextToken().equals("Endless_hard"))
+			else if(type.equals("Endless_hard"))
 				endlesshard = Integer.parseInt(tk.nextToken());
-			else if(tk.nextToken().equals("Efficiency_easy"))
+			else if(type.equals("Efficiency_easy"))
 				effeasy = Double.parseDouble(tk.nextToken());			
-			else if(tk.nextToken().equals("Efficiency_medium"))
+			else if(type.equals("Efficiency_medium"))
 				effmedium = Double.parseDouble(tk.nextToken());	
-			else if(tk.nextToken().equals("Efficiency_hard"))
+			else if(type.equals("Efficiency_hard"))
 				effhard = Double.parseDouble(tk.nextToken());
 			try {
 				contents = br.readLine();
@@ -94,5 +97,116 @@ public class SaveFile
 		System.out.println("Efficiency Easy Score " + effeasy);
 		System.out.println("Efficiency Medium Score " + effmedium);
 		System.out.println("Efficiency Hard Score " + effhard);
+	}
+	
+	public int getEndlessEasy()
+	{
+		return endlesseasy;
+	}
+	
+	public int getEndlessMedium()
+	{
+		return endlessmed;
+	}
+	
+	public int getEndlessHard()
+	{
+		return endlesshard;
+	}
+	
+	public double getEfficiencyEasy()
+	{
+		return effeasy;
+	}
+	
+	public double getEfficiencyMedium()
+	{
+		return effmedium;
+	}
+	
+	public double getEfficiencyHard()
+	{
+		return effhard;
+	}
+	
+	public void setEndlessEasy(int endlesseasy)
+	{
+		this.endlesseasy =  endlesseasy;
+	}
+	
+	public void setEndlessMedium(int endlessmed)
+	{
+		this.endlessmed =  endlessmed;
+	}
+	
+	public void setEndlessHard(int endlesshard)
+	{
+		this.endlesshard = endlesshard;
+	}
+	
+	public void setEfficiencyEasy(double effeasy)
+	{
+		this.effeasy = effeasy;
+	}
+	
+	public void setEfficiencyMedium(double effmedium)
+	{
+		this.effmedium = effmedium;
+	}
+	
+	public void setEfficiencyHard(double effhard)
+	{
+		this.effhard = effhard;
+	}
+	
+	public void Savegame() throws IOException
+	{
+		File directoryFile = new File("saveDirectory.txt");
+		FileReader fr = null;
+		try {
+			fr = new FileReader(directoryFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		BufferedReader br = new BufferedReader(fr);
+		
+		String path = null;
+		try {
+			path = br.readLine() + "\\" + LoginFrame.savedfilename + ".txt";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		File information = new File(path);
+		FileWriter fw = null;
+			 try {
+				fw = new FileWriter(information);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.append("Username " + name);
+		bw.newLine();
+		bw.append("Password " + password);
+		bw.newLine();
+		bw.append("Color " + color);
+		bw.newLine();
+		bw.append("Endless_easy " + endlesseasy);
+		bw.newLine();
+		bw.append("Endless_medium " + endlessmed);
+		bw.newLine();
+		bw.append("Endless_hard " + endlesshard);
+		bw.newLine();
+		bw.append("Efficiency_easy " + effeasy);
+		bw.newLine();
+		bw.append("Efficiency_medium " + effmedium);
+		bw.newLine();
+		bw.append("Efficiency_hard " + effhard);
+		bw.close();
 	}
 }
